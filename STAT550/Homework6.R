b2fd96f7c1d009fb7d10768a1270639dda71e4be
BoostedJ <justin.pelak7@gmail.com>
2025-02-05 19:25:44 -0800

    updated hw

--- STAT550/Homework6.R
+++ STAT550/Homework6.R
@@ -19,8 +19,28 @@ mean(simlist)
 # 1.43 Use R to simulate the probability of getting at least 8 in the sum of two
 # dice rolls.
 
+n = 10000
+simlist = numeric(n)
+dice=c(1,2,3,4,5,6)
+for (i in 1:n) {
+  trial = sample(dice, 2, replace=T)
+  success = if (sum(trial)>=8) 1 else 0
+  simlist[i] = success
+}
+mean(simlist)
+
 # 1.44 Use R to simulate the probability in Exercise 1.30.
 # (1.30 A tetrahedron dice is four-sided and labeled with 1, 2, 3, and 4.
 # When rolled it lands on the base of a pyramid and the number rolled is the
 # number on the base. In five rolls, what is the probability of
 # rolling at least one 2?)
+
+n = 10000
+simlist = numeric(n)
+dice = c(1,2,3,4)
+for (i in 1:n) {
+  trial = sample(dice, 5, replace=T)
+  success = if (2 %in% trial) 1 else 0
+  simlist[i] = success
+}
+mean(simlist)
